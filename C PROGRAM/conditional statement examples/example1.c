708d6de1ea1e1673050ee6e23ad5eb71f3dfe220
SivaMurugan04 <sivamurugan04012004@gmail.com>
2024-10-18 22:19:27 +0530

    example1

---
+++ C PROGRAM/conditional statement examples/example1.c
@@ -0,0 +1,12 @@
+//Write C program to accept two intergs and check whether they are equal or not equal
+#include <stdio.h>
+int main()
+{
+int a,b;
+scanf("%d%d",&a,&b);
+if(a==b)
+{printf("Two number is equal");}
+else
+{printf("Two number not equal");}
+
+}
